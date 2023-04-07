6d9f13dfe024dc17c2d34efa4b956b3a3dc008c0
gluvaja <lukagluvajic@gmail.com>
2023-04-07 17:26:22 +0200

    ledInit function added

--- main.c
+++ main.c
@@ -9,20 +9,28 @@
 #include <util/delay.h>
 #include <stdint.h>
 
+void ledBlink(int16_t high_time, int16_t low_time)
+{
+	PORTB |= 1 << 5;
+	_delay_ms(high_time);
+	PORTB &= ~(1 << 5);
+	_delay_ms(low_time);
+}
+
+void ledInit()
+{
+	DDRB |= 1 << 5;
+}
 int16_t main()
 {
 	int16_t high_time = 300;
 	int16_t low_time = 700;
 
-	DDRB |= 1 << 5;
-
+	ledInit();
 
 	while(1)
 	{
-		PORTB |= 1 << 5;
-		_delay_ms(high_time);
-		PORTB &= ~(1 << 5);
-		_delay_ms(low_time);
+		ledBlink(high_time, low_time);
 	}
 
 	return 0;
