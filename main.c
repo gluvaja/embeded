8a04601b03b3e70689cd8bdf01113ae95ca54318
gluvaja <lukagluvajic@gmail.com>
2023-04-07 17:17:04 +0200

    Initial code added

--- main.c
+++ main.c
@@ -6,12 +6,25 @@
  */
 
 #include <avr/io.h>
+#include <util/delay.h>
 #include <stdint.h>
 
 int16_t main()
 {
+	int16_t high_time = 500;
+	int16_t low_time = 500;
+
+	DDRB |= 1 << 5;
+
+
 	while(1)
-		;
+	{
+		PORTB |= 1 << 5;
+		_delay_ms(high_time);
+		PORTB &= ~(1 << 5);
+		_delay_ms(low_time);
+	}
+
 	return 0;
 }
 
